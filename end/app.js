e01df506444030a267ea485eac164145c18cdbee
jochris15 <jochris15@gmail.com>
2024-06-27 11:41:14 +0700

    change back from deploy

--- end/app.js
+++ end/app.js
@@ -1,10 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
 const Controller = require('./controllers/controller')
 
 app.use(express.urlencoded({ extended: true }))
